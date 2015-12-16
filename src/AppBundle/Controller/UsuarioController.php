2dffa12c5f25d5824a1db01620d4b094bcdb6297
Pedro Quesada Marin <quesada1992@hotmail.com>
2015-12-16 18:36:56 +0100

    Ahora se puede crear un usuario desde el login

--- src/AppBundle/Controller/UsuarioController.php
+++ src/AppBundle/Controller/UsuarioController.php
@@ -28,7 +28,45 @@ class UsuarioController extends Controller
             'usuarios' => $usuarios
         ]);
     }
+    /**
+     * @Route("/nuevaCuenta", name="nueva_cuenta")
+     */
+    public function nuevaCuentaAction(Request $peticion)
+    {
+        $usuario = new Usuario();
 
+        // Crear el formulario a partir de la clase
+        $formulario = $this->createForm(new UsuarioType(), $usuario);
+
+        // Procesar el formulario si se ha enviado con un POST
+        $formulario->handleRequest($peticion);
+
+        // Si se ha enviado y el contenido es vÃ¡lido, guardar los cambios
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            $usuario->setEsAdmin(false);
+            $usuario->setEsCamarero(false);
+            $usuario->setEsCliente(true);
+
+            // Obtener el EntityManager
+            $em = $this->getDoctrine()->getManager();
+
+            // $helper = $password = $this->container->get('security.password_encoder');
+            //$usuario->setPass($helper->encodePassword($usuario, $usuario->getPass()));
+
+            $em->persist($usuario);
+            // Guardar los cambios
+            $em->flush();
+
+            // Redirigir al usuario a la lista
+            return new RedirectResponse(
+                $this->generateUrl('inicio')
+            );
+        }
+        return $this->render(':usuario:nueva_cuenta.html.twig' ,[
+            'usuario' => $usuario,
+            'formulario' => $formulario->createView()
+        ]);
+    }
 
     /**
      * @Route("/nuevo", name="usuarios_nuevo")
@@ -43,7 +81,7 @@ class UsuarioController extends Controller
         // Procesar el formulario si se ha enviado con un POST
         $formulario->handleRequest($peticion);
 
-        // Si se ha enviado y el contenido es válido, guardar los cambios
+        // Si se ha enviado y el contenido es vÃ¡lido, guardar los cambios
        if ($formulario->isSubmitted() && $formulario->isValid()) {
 
            // Obtener el EntityManager
@@ -77,7 +115,7 @@ class UsuarioController extends Controller
                 // Procesar el formulario si se ha enviado con un POST
                 $formulario->handleRequest($peticion);
 
-                // Si se ha enviado y el contenido es válido, guardar los cambios
+                // Si se ha enviado y el contenido es vÃ¡lido, guardar los cambios
                 if ($formulario->isSubmitted() && $formulario->isValid()) {
 
                     // Obtener el EntityManager
@@ -105,7 +143,7 @@ class UsuarioController extends Controller
     /**
      * @Route("/eliminar/{usuario}", name="usuarios_eliminar")
      */
-    public function eliminarAction(Request $peticion, Usuario $usuario)
+    public function eliminarAction(Usuario $usuario)
     {
 
         //Eliminar usuario
@@ -114,7 +152,6 @@ class UsuarioController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->remove($usuario);
             $em->flush();
-            $this->addFlash('success', 'Usuario eliminado de forma correcta');
 
 
             // Redirigir al usuario a la lista
